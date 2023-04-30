9325504664853c7f9c2268aff32f87a0d8ab3110
pratheeshrk451 <pratheeshrk451@gmail.com>
2023-04-30 11:40:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import Article from './components/article/Article';
+// import Brand from './components/brand/Brand';
+// import CTA from './components/cta/CTA';
+// import Feature from './components/Feature';
+// import Navbar from './components/navbar/Navbar';
+//import {Article, Brand, CTA, Feature, Navbar} from './components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Blog, Footer, Header, Possibility, Features, WhatGPT3 } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css'
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <h1>Hello all</h1>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+
+    )
 }
-
 export default App;
